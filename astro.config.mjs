1cf882b4453b2c7eb59a9eb4df8a2f01673def46
Henrik S. Zimmermann <henrik.zimmermann@utoronto.ca>
2025-03-01 21:17:50 -0500

    Added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import relativeLinks from 'astro-relative-links';
+import tailwindcss from "@tailwindcss/vite";
 
 import mdx from '@astrojs/mdx';
 import rehypeCitations from './src/lib/citation-extractor';
@@ -11,7 +12,7 @@ export default defineConfig({
   integrations: [relativeLinks(), mdx()],
 
   vite: {
-    plugins: []
+    plugins: [tailwindcss()]
   },
   markdown: {
     rehypePlugins: [rehypeCitations]
